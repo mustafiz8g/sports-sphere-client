13eb9e41e71d9672b918bb6387651d1f3436b94e
mustafiz8g <mustafizur8g@gmail.com>
2024-12-04 18:45:34 +0600

    project setup primarily

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer";
+import Navbar from "../Shared/Navbar";
+
+
+
+const MainLayout = () => {
+    return (
+        <div>
+          <Navbar></Navbar>
+          <Outlet></Outlet>
+          <Footer></Footer>
+
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+const Home = () => {
+    return (
+        <div>
+            this is home
+         
+        </div>
+    );
+};
+
+export default Home;
